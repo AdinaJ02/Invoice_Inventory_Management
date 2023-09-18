259cba44408b8d86933d7e4624a663161cf22da5
Kishan Chaurasia <chaurasiak052@gmail.com>
2023-09-18 15:11:48 +0530

    login page frontend

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
-<?php 
-    
+<?php
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="index.css">
+    <title>Login Page</title>
 </head>
+
 <body>
-    <h1>Helllo World</h1>
+    <div class="login">
+        <div class='form'>
+            <form>
+                <input type="text" placeholder='Email/Phone' class='text' id='username' required><br>
+                <input type="password" placeholder='Password' class='password'><br>
+                <a href="#" class='btn-login' id='do-login'>Login</a>
+                <a href="#" class='forgot'>Forgot Password?</a>
+            </form>
+        </div>
+    </div>
     <script src="index.js"></script>
 </body>
+
 </html>
